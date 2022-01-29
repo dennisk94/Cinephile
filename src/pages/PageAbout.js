c2810399d2c58038e0f13e5978c3824674663f94
dennisk94 <87791631+dennisk94@users.noreply.github.com>
2022-01-29 00:20:59 -0800

    About Page

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,8 +1,18 @@
+import TMDB from '../images/tmdb.svg';
+import { Link } from 'react-router-dom';
+
 function PageAbout() {
     return (
   
-      <div>
-         <p>This is the about page</p>
+      <div className="page-about">
+        <h2>About Us</h2>
+        <div className="page-about-card">
+          <p>Welcome to Cinephile database. Here you will find a variety of movies. Enjoy browsing and learning about the most talked about movies.</p>
+          <p>Movie Pulse uses the TMDB API but is not endorsed or certified by TMBD.</p>
+          <div className="tmdb-attribution">
+          <Link to={{ pathname: "https://www.themoviedb.org/" }} target="_blank"  ><img src={ TMDB } alt="TMDB Logo" /></Link>
+          </div>
+        </div>
       </div>
     )
   }
